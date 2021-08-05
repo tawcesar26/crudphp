e360ee230d1bb8aa6903220ce844a157a4b11ef6
tawcesar26 <tawcesar26@hotmail.com>
2021-08-05 00:08:48 -0300

    Estrutura do CI

---
+++ application/models/Exemplo1_model.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Exemplo1_model extends CI_Model {
+
+	function __construct(){
+
+		parent::__construct();
+
+	}
+
+	public function cadastrar(){
+
+		echo "Cadastro realizado com sucesso!";
+
+
+	}
+	public function atualizar(){
+
+		
+	}
+	public function listar(){
+
+		
+	}
+	public function deletar(){
+
+		
+	}
+
+
+
+}
+
+
+
+?>
